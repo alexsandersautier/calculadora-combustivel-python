1bb956dd9dbbbb88a61d844f862b9598bdadc3df
Alexsander Sautier <alexsanderesramos@gmail.com>
2022-09-19 18:23:20 -0300

    interface pronta

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Esta aplicação tem como finalidade demonstrar os valores que serão gastos'
+      'com combustível durante uma viagem, com base no consumo do seu veículo, e'
+      'com a distância determinada por você!')
+print('Os combustíveis disponíveis para este cálculo são:')
+print('\nºGasolina'
+      '\nºÁlcool'
+      '\nºDiesel')
+print('Distância em a ser percorrida(KM):')
+print('Consumo de combustível do veículo(KM/L: ')
+print('O valor total gasto será de:')
+print('\nGasolina:'
+      '\nÁlcool:'
+      '\nDiesel:')
